1dfce8e164391fa3e139e9820c4ebdf7fa9f8d9c
Lucas Peralta <lp954982@gmail.com>
2024-02-24 09:02:55 -0300

    primer commit trabajo practico

---
+++ trabajo-práctico-nº1-java-jedi/iniciado/Ejercicio_1.java
@@ -0,0 +1,10 @@
+public class Ejercicio_1 {
+    public static void main(String[] args) {
+    //Suma de dos números: Se le solicita que guarden dos números enteros y los sumen. El resultado mostrarlo por pantalla.
+    int numero1 = 10;
+    int numero2 = 13;
+    int resultado = numero1 + numero2;
+    System.out.println("El resultado de la suma es: " + resultado);
+    }
+    
+}
