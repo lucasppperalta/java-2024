062c8f07fdad5bc9743aee42b26d97afb9a72311
Lucas Peralta <lp954982@gmail.com>
2024-02-28 09:22:38 -0300

    primeros ejercicios padawan

---
+++ trabajo-práctico-nº1-java-jedi/padawan/Ejercicio_padawan_2.java
@@ -0,0 +1,10 @@
+public class Ejercicio_padawan_2 {
+    public static void main(String[] args) {
+        /*Descontador: Se le solicita que guarde un número en un espacio de memoria y
+        discretamente dicho número 20 veces sin usar asignación. ¿Que operador podría
+        usar para hacerlo? */
+        int numero = 20;
+        numero -= 20;
+        System.out.println(numero);
+    }
+}
