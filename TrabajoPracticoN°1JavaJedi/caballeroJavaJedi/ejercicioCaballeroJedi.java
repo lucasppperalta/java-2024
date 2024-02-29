41f9df1a23616e8321fed9bb04dd8762701b79b6
Lucas Peralta <lp954982@gmail.com>
2024-02-29 19:30:37 -0300

    caballero Jedi

---
+++ TrabajoPracticoN°1JavaJedi/caballeroJavaJedi/ejercicioCaballeroJedi.java
@@ -0,0 +1,13 @@
+package caballeroJavaJedi;
+
+public class ejercicioCaballeroJedi {
+    public static void main(String[] args) {
+        /* Obtener porcentaje : Dados dos números, uno real y el otro entero. Obtenga el
+        porcentaje que indique el segundo número del primer número. */
+        int numero1 = 80;
+        double numero2 = 40.5;
+        double porcentaje = (numero2 / numero1) * 100;
+        System.out.println("El porcentaje que indica el segundo número del primer número es %" + porcentaje);
+
+    }
+}
