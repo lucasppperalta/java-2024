a7d404bfb5015227edb8f237fb039cae133c9f78
Lucas Peralta <lp954982@gmail.com>
2024-03-04 10:09:45 -0300

    ejercicios maestro jedi

---
+++ TrabajoPracticoN°1JavaJedi/maestroJavaJedi/ejercicioMaestroJedi3.java
@@ -0,0 +1,9 @@
+public class ejercicioMaestroJedi3 {
+    public static void main(String[] args) {
+        /*Cálculo de la hipotenusa de un triángulo:
+        Escribir un programa en Java que calcule la hipotenusa de un triángulo rectángulo,
+        dados los valores de los catetos. Utilizar la fórmula: h^2 = a^2 + b^2, donde h es la
+        hipotenusa, a y b son los catetos. */
+    }
+
+}
---
+++ TrabajoPracticoN°1JavaJedi/maestroJavaJedi/ejercicioMaestroJedi4.java
@@ -0,0 +1,8 @@
+public class ejercicioMaestroJedi4 {
+    public static void main(String[] args) {
+        /*Escribir un programa en Java que calcule la media aritmética de un conjunto de
+        números. Utilizar la fórmula: media = (n1 + n2 + ... + nk) / k, donde n1, n2, ..., nk son
+        los números y k es el número total de elementos. Utilicen variables y constantes
+        según corresponda. */
+    }
+}
