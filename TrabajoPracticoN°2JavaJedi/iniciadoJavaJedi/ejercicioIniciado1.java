90260d230f9cf25af33fd906c290843df8fdba0b
Lucas Peralta <lp954982@gmail.com>
2024-03-04 18:35:06 -0300

    primer commit

---
+++ TrabajoPracticoN°2JavaJedi/iniciadoJavaJedi/ejercicioIniciado1.java
@@ -0,0 +1,21 @@
+package iniciadoJavaJedi;
+
+public class ejercicioIniciado1 {
+    public static void main(String[] args) {
+        /*Triangulo de asteriscos : Se le solicita que creen un programa que imprima un
+        triángulo de asteriscos en la consola. Usar bucles.*/
+        String triangulo = "";
+
+        int index = 1;
+        int base = 10;
+
+        while (index <= base) {
+            String estrellas = "*".repeat(index);
+            estrellas = estrellas + " ".repeat(base - index) + "\n";
+            triangulo = triangulo + estrellas;
+            index++;
+        }
+
+        System.out.println(triangulo);
+    }
+}
