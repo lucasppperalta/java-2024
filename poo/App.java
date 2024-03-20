f184569ca160592d2cba609374e41551de386c05
Lucas Peralta <lp954982@gmail.com>
2024-03-19 21:54:30 -0300

    segunda parte de clases poo

--- poo/App.java
+++ poo/App.java
@@ -4,14 +4,26 @@ public class App {
     public static void main(String[] args) {
         // new devuelve la dirección de memoria que hace referencia al objeto creado
         Persona persona = new Persona(); // Instancia de un objeto
-        persona.nombre = "Lucas";
+
+        // Asignacion de valores 
+        /*persona.nombre = "Lucas";
         persona.apellido = "Peralta";
         persona.edad = 27;
 
+
+        // Acceso a la informacion del objeto
         System.out.println(persona.nombre);
         System.out.println(persona.apellido);
-        System.out.println(persona.edad);
+        System.out.println(persona.edad);*/
 
+        persona.setNombre("Lucas");
+        persona.setApellido("Peralta");
+        persona.setEdad(27);
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getApellido());
+        System.out.println(persona.getEdad());
+        persona.setNombre("Rodrigo");
+        System.out.println(persona.getNombre());
 
     }
 }
--- poo/Persona.java
+++ poo/Persona.java
@@ -2,8 +2,38 @@ package poo;
 
 public class Persona {
         //Atributos no variables. Porque son atributos o informacion de la clase
-        String nombre;
-        String apellido;
-        int edad;
-    
+        private String nombre;
+        private String apellido;
+        private int edad;
+        
+        // Metodos
+        //[clasificador] [tipoDeRetorno] nombreDelMetodo([<expresiones>])){}
+        // puede ser public 
+
+        public String getNombre (){
+                return this.nombre; //this hace referencia a sí mismo
+        }
+
+        public String getApellido(){
+                return this.apellido;
+        }
+
+        public int getEdad(){
+                return this.edad;
+        }
+
+        public void setNombre(String nombre){
+                if(this.nombre == null){ // Para que no se pueda modificar luego el nombre
+                        this.nombre = nombre;
+                }
+                
+        }
+
+        public void setApellido(String apellido){
+                this.apellido= apellido;
+        }
+        public void setEdad(int edad){
+                this.edad = edad;
+        }
+
 }
